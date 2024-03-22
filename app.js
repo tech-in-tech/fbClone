3b4d07b182f861169b4a2381f8779ea7d77be17d
tech-in-tech <anubhavsharma06dec@gmail.com>
2024-03-22 23:32:23 +0530

    add settings menu section

---
+++ app.js
@@ -0,0 +1,6 @@
+let profile = document.querySelector(".nav-user-icon");
+let settingsMenu = document.querySelector(".setting-menu")
+profile.addEventListener("click",(e)=>{
+  settingsMenu.classList.toggle("setting-menu-height")
+})
+
